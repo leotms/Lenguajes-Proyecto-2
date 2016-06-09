69b861c25ea20c31140c8a800a6a7223ba2f2232
Leonardo Martinez <martinezazuaje@gmail.com>
2016-06-09 00:10:04 -0400

    Inicio de Arboles como Listas.

--- arboles.pl
+++ arboles.pl
@@ -1,4 +1,17 @@
-% Arboles como estructura.
+% Autores:
+% Aldrix Marfil     10-10940.
+% Leonardo Martinez 11-10576.
 
+% Arboles como Estructuras
 nodo(Etiqueta,[]) :- integer(Etiqueta).
 nodo(Etiqueta,[X|Y]) :- integer(Etiqueta).
+
+
+%Arboles como Lista
+li(X)        :- list(X).
+li([X])      :- integer(X).
+esq([X|Y]) :- li(X), esq(Y).
+
+listaEntero(X) :-   list(X).
+listaEntero([X]) :- integer(X).
+listaEntero([X|Y]) :- integer(X), listaEntero(Y).
