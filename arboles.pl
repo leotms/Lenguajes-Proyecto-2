f30c096eaf100e3d1b565cdd500cc4ed9be541ae
aldrix <aldrix_marfil@hotmail.com>
2016-06-08 23:17:06 -0400

    Reconoce la estructura de arbol basada en nodo(Etiqueta,listaSubarboles)

--- arboles.pl
+++ arboles.pl
@@ -1,6 +1,4 @@
 % Arboles como estructura.
 
-nodo(E,L).
-arista(E1,nodo(E2,L)).
-nodo(Etiqueta,[]).
-nodo(Etiqueta,[X|Y]).
+nodo(Etiqueta,[]) :- integer(Etiqueta).
+nodo(Etiqueta,[X|Y]) :- integer(Etiqueta).
