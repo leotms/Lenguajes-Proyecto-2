0a99c9a913e46a3153190cea1ab580401175a2b8
aldrix <aldrix_marfil@hotmail.com>
2016-06-09 00:02:16 -0400

    Iniciada implementacion del predicado bienEtiquetado

--- arboles.pl
+++ arboles.pl
@@ -1,4 +1,11 @@
 % Arboles como estructura.
 
 nodo(Etiqueta,[]) :- integer(Etiqueta).
-nodo(Etiqueta,[X|Y]) :- integer(Etiqueta).
+nodo(Etiqueta,[X|Y]) :- integer(Etiqueta), .
+
+
+% Predicado que indica si los nodos de un arbol estan bien etiquetados.
+
+bienEtiquetado(+Arbol). 
+bienEtiquetado(nodo(Etiqueta,[])).
+bienEtiquetado(nodo(Etiqueta,[X|Y])) :-  
