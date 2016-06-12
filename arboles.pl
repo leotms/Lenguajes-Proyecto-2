19ce65c2cb030ca101d51f981cdc925b746e2c68
aldrix <aldrix_marfil@hotmail.com>
2016-06-12 19:00:08 -0400

    Agregada la funcion bienEtiquetado2 que dado un arbol devuelve todos los nodos y las aristas en listas diferentes respectivamente.

--- arboles.pl
+++ arboles.pl
@@ -9,29 +9,30 @@
 
 esVacia([]).
 
-noEstaEnLaLista(X,[Y|Z]) :- integer(X), integer(Y), esVacia(Z), X =\= Y.
+noEstaEnLaLista(X,[]) :- integer(X).
 noEstaEnLaLista(X,[Y|Z]) :- integer(X), integer(Y), X =\= Y, noEstaEnLaLista(X,Z).
 
-insertarEnLaLista(X,Y,[X]) :- esVacia(Y).
-insertarEnLaLista(X,Y,[X|Y]) :- noEstaEnLaLista(X,Y).
+insertarEnLaLista(X,[],[X]).
+insertarEnLaLista(X,Y,[X|Y]). % :- noEstaEnLaLista(X,Y).
 
 
 % ----------------------------- bienEtiquetado --------------------------------
 % Predicado que indica si los nodos de un arbol estan bien etiquetados.
 
-bienEtiquetado2(nodo(E,X),L1,L2) :- integer(E), esVacia(X), esVacia(L1), esVacia(L2).
+bienEtiquetado2(nodo(E,[]),LN,LA,RN,LA) :- integer(E), 
+		insertarEnLaLista(E,LN,RN).
 
+bienEtiquetado2(nodo(EP, arista(Y,nodo(EH,Z))),LN,LA,MN,MA) :-
+	   integer(EP), integer(Y), integer(EH), Y =:= EP - EH, 
+	   insertarEnLaLista(Y,LA,RA),
+       bienEtiquetado2(nodo(EH,Z),LN,RA,MN,MA).
 
-bienEtiquetado2(nodo(EP, arista(Y,nodo(EH,Z)))) :-
-	    integer(EP), integer(Y), integer(EH), Y =:= EP - EH,
-        bienEtiquetado2(nodo(EH,Z)).
-
-bienEtiquetado2(nodo(E,[X|XS])) :-
-        bienEtiquetado2(nodo(E,X)),!,
-        bienEtiquetado2(nodo(E,XS)).
+bienEtiquetado2(nodo(E,[X|XS]),LN,LA,XN,XA) :-
+        bienEtiquetado2(nodo(E,X),LN,LA,RN,RA),!,
+        bienEtiquetado2(nodo(E,XS),RN,RA,XN,XA).
 
 % ------------- Predicado Principal --------------
-bienEtiquetado(Arbol) :- bienEtiquetado2(Arbol,[],[]).
+bienEtiquetado2(Arbol,[],[],RN,RA).
 
 % Arboles como Listas
 
