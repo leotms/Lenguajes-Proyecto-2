842fa92b1fcfe1e7efc4b89eb5f06ed146099f58
aldrix <aldrix_marfil@hotmail.com>
2016-06-15 14:12:45 -0400

    Funciona esqueleto. Codigo refactorizado.

--- arboles.pl
+++ arboles.pl
@@ -1,17 +1,11 @@
-% -----------------------------------------------------------------------------
-% Autores:
-% Aldrix Marfil     10-10940.
-% Leonardo Martinez 11-10576.
-% -----------------------------------------------------------------------------
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Autores:																	  %
+% Aldrix Marfil     10-10940.												  %
+% Leonardo Martinez 11-10576.												  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-% Arboles como Estructuras
-% nodo(Etiqueta,[]) :- integer(Etiqueta).
-% nodo(Etiqueta,[X|Y]) :- integer(Etiqueta).
-% arista(Etiqueta, Nodo) :- integer(Etiqueta) , nodo(Nodo).
-
-
-% ------------------------- Predicados Auxiliares -----------------------------
+% ------------------------ Auxiliares bienEtiquetado --------------------------
 
 % Permite saber el cual es el maximo entre dos numeros.
 maximo(X,Y,R) :- X>Y, R is X,!.
@@ -23,7 +17,8 @@ maximoDeLaLista(I,[X|Y],R) :- maximo(I,X,AuxR), maximoDeLaLista(AuxR,Y,R).
 
 % Permite saber si un elemento no es miembro de una lista.
 noEstaEnLaLista(X,[]) :- integer(X).
-noEstaEnLaLista(X,[Y|Z]) :- integer(X), integer(Y), X =\= Y, noEstaEnLaLista(X,Z).
+noEstaEnLaLista(X,[Y|Z]) :- integer(X), integer(Y), X =\= Y, 
+							noEstaEnLaLista(X,Z).
 
 % Inserta un elemento X al inicio de la lista pasada como segundo argumento.
 insertarEnLaLista(X,[],[X]).
@@ -31,49 +26,147 @@ insertarEnLaLista(X,Y,[X|Y]) :- noEstaEnLaLista(X,Y).
 
 
 % ----------------------------- bienEtiquetado --------------------------------
+% =============================================================================
 % Predicado que indica si los nodos de un arbol estan bien etiquetados.
-% Tanto las etiquetas de las aristas como las de los nodos deben ser distintas.
+% Parametros: Arbol - Estructura de arbol representada como
+					  nodo(EN1,[arista(EA,nodo(EN2,[])))
 
-bienEtiquetado2(nodo(E,[]),LN,LA,RN,LA) :-
+bienEtiquetadoAux(nodo(E,[]),LN,LA,RN,LA) :-
 		integer(E), E > 0,
 		insertarEnLaLista(E,LN,RN).
 
-bienEtiquetado2(nodo(EP,arista(Y,nodo(EH,Z))),LN,LA,RN,RA) :-
+bienEtiquetadoAux(nodo(EP,arista(Y,nodo(EH,Z))),LN,LA,RN,RA) :-
 	    integer(EP), integer(Y), integer(EH),
 	    Y =:= abs(EP - EH), Y > 0,
 	    insertarEnLaLista(Y,LA,AuxA),
-        bienEtiquetado2(nodo(EH,Z),LN,AuxA,RN,RA).
+        bienEtiquetadoAux(nodo(EH,Z),LN,AuxA,RN,RA).
 
-bienEtiquetado2(nodo(E,[X|XS]),LN,LA,RN,RA) :-
-        bienEtiquetado2(nodo(E,X),LN,LA,AuxN,AuxA), !,
-        bienEtiquetado2(nodo(E,XS),AuxN,AuxA,RN,RA).
+bienEtiquetadoAux(nodo(E,[X|XS]),LN,LA,RN,RA) :-
+        bienEtiquetadoAux(nodo(E,X),LN,LA,AuxN,AuxA), !,
+        bienEtiquetadoAux(nodo(E,XS),AuxN,AuxA,RN,RA).
 
 % -------- Predicado Principal --------
 bienEtiquetado(Arbol) :-
-		bienEtiquetado2(Arbol,[],[],RN,RA), !, length(RN,X),
+		bienEtiquetadoAux(Arbol,[],[],RN,RA), !, length(RN,X),
 		maximoDeLaLista(0,RN,MaxN), MaxN =< X,
 		maximoDeLaLista(0,RA,MaxA), MaxA =< X - 1, !.
 
 
-% ----------------------- Describir Etiquetamiento ----------------------------
+% ------------------------ Auxiliares esqueleto -------------------------------
 
-describirEtiquetamiento2(nodo(E,[]),NpID, _NherID) :-
-			escribirNodo(NpID,E), write('\n').
+% Permite generar listas con todas las combinaciones entre 0 un maximo.
+generarLista(0,_,L,L).
 
-describirEtiquetamiento2(nodo(_EP,arista(Y,nodo(EH,Z))), NpID, NherID) :-
-			concatenarIdentificador(NpID,NherID,NewID),
-			escribirArista(Y),
-			describirEtiquetamiento2(nodo(EH,Z),NewID,0).
+generarLista(TAM,MAX,L,LR) :- TAM>0, between(0,MAX,X), append(L,[X],AuxL), 
+ 							  R is TAM-1, generarLista(R,MAX,AuxL,LR).
 
-describirEtiquetamiento2(nodo(E,[X|XS]),NpID, NherID) :-
-			escribirNodo(NpID,E),!,
-			describirEtiquetamiento2(nodo(E,X),NpID,NherID),
-			Naux is NherID + 1,
-			describirEtiquetamiento2(nodo(E,XS),NpID, Naux),!.
+lista(TAM,MAX,L) :- generarLista(TAM,MAX,[],L).
 
+
+% Permite verificar que una lista es no creciente.
+verificarListaNoCreciente(L) :- length(L,T), T < 2.
+
+verificarListaNoCreciente([X0,X1|XS]) :- X0 >= X1, 
+                                         verificarListaNoCreciente([X1|XS]).
+
+
+% ------------------------------- esqueleto -----------------------------------
+% =============================================================================
+% Permite calcular representaciones de arboles, no ordenados, como listas.
+% Parametros: N         - Numero de nodos que posee el arbol.
+%             R         - Cantidad maxima de hijos que puede tener un nodo.
+%             Esqueleto - Arbol representado como lista para una configuracion 
+%                         especifica.
+
+esqueletoAux(1,_,L,ESQ,TamL) :- 
+		lista(TamL,0,AuxL), append(L,[AuxL],ESQ).
+
+esqueletoAux(N,R,L,ESQ,TamL) :- 
+		N > 1, lista(TamL,R,AuxL1), 
+		verificarListaNoCreciente(AuxL1), 
+		sum_list(AuxL1,AuxSumL),   % Sumamos para saber el tamano de la lista de los hijos.
+		AuxSumL > 0, 
+		append(L,[AuxL1],AuxL2),   
+		AuxN is N-AuxSumL,         % Disminuimos el numero de nodos.
+		esqueletoAux(AuxN,R,AuxL2,ESQ,AuxSumL).
 
 % -------- Predicado Principal --------
-describirEtiquetamiento(Arbol) :- describirEtiquetamiento2(Arbol,'0.',0).
+esqueleto(1,_,[[0]]).
+esqueleto(N,R,ESQ) :- N > 1, esqueletoAux(N,R,[],ESQ,1).
+ 
+
+% --------------------------- etiquetamiento ----------------------------------
+% =============================================================================
+% Permite saber si Arbol es un buen etiquetamiento de Esqueleto.
+% Parametros: Esqueleto - Arbol representado como lista para una configuracion 
+%                         especifica.
+%             Arbol     - Arbol - Estructura de arbol representada como
+					      nodo(EN1,[arista(EA,nodo(EN2,[])))
+
+% insertarAlPrincipioLista([],_,[]).
+% insertarAlPrincipioLista(X,[],[X]).
+% insertarAlPrincipioLista(X,Y,[X|Y]).
+
+% concatenarListas(L1,[],L1).
+% concatenarListas([],L2,L2).
+% concatenarListas([L|LS],LR,[L|Z]) :- concatenarListas(LS,LR,Z).
+
+
+
+% unirSubEsqueletos([X|XS],[Y|YS],ESQ) :- 
+%	concatenarListas(X,Y,Aux1),
+%	(noVacia(XS), noVacia(YS) -> unirSubEsqueletos(XS,YS,Aux2); true),
+%	insertarAlPrincipioLista(Aux2,[],AuxESQ),
+%	insertarAlPrincipioLista(Aux1,AuxESQ,ESQ).
+
+% noVacia(L) :- length(L,T), T > 0.
+
+
+% visitarAristas([],_,[]).
+% visitarAristas([arista(E,N)|XS],L,ESQ) :- 
+%	    bienEtiquetadoAEsqueleto(N,AuxESQ),
+%	    (noVacia(XS) -> visitarAristas(XS,AuxESQ,ESQ); true).
+
+% bienEtiquetadoAEsqueleto(nodo(E,[]),ESQ) :-
+%		ESQ = [0].
+
+% bienEtiquetadoAEsqueleto(nodo(E,LA),ESQ) :-
+%		length(LA,T),
+%		visitarAristas(LA,[],SubESQ),
+%		insertarAlPrincipioLista([T],SubESQ,ESQ).
+
+% etiquetamiento(Esqueleto,Arbol) :- bienEtiquetadoAEsqueleto(Arbol,ESQ).
+
+
+% ---------------------------- esqtiquetable ----------------------------------
+% =============================================================================
+% Permite saber si todos los esqueletos de arboles R-arios con N nodos son bien
+% etiquetables.
+% Parametros: Arbol -
+
+
+% ------------------------ describirEtiquetamiento ----------------------------
+% =============================================================================
+% Permite mostrar en pantalla la descripcion de un Arbol y su etiquetamiento.
+% Parametros: Arbol - Estructura de arbol representada como
+					  nodo(EN1,[arista(EA,nodo(EN2,[])))
+
+describirEtiquetamientoAux(nodo(E,[]),NpID, _NherID) :-
+			escribirNodo(NpID,E), write('\n').
+
+describirEtiquetamientoAux(nodo(_EP,arista(Y,nodo(EH,Z))), NpID, NherID) :-
+			concatenarIdentificador(NpID,NherID,NewID),
+			escribirArista(Y),
+			describirEtiquetamientoAux(nodo(EH,Z),NewID,0).
+
+describirEtiquetamientoAux(nodo(E,[X|XS]),NpID, NherID) :-
+			escribirNodo(NpID,E),!,
+			describirEtiquetamientoAux(nodo(E,X),NpID,NherID),
+			Naux is NherID + 1,
+			describirEtiquetamientoAux(nodo(E,XS),NpID, Naux),!.
+
+% -------- Predicado Principal --------
+describirEtiquetamiento(Arbol) :- describirEtiquetamientoAux(Arbol,'0.',0).
 
 escribirNodo(Nid,E) :- write(Nid), write(' ('), write(E), write(') ').
 
@@ -82,27 +175,3 @@ escribirArista(E)   :- write(' -- '), write(E), write(' -- ').
 concatenarIdentificador(ID1, ID2, X) :-
 			atom_concat(ID1,'.',X2),
 			atom_concat(X2,ID2,X).
-
-% ------------------------------- Esq -----------------------------------------
-
-% Arboles como listas
-
-agregarEsqueleto(X,V, Esq) :- insertarEnLaLista(V,X,EsqAux), Esq is EsqAux.
-esqueleto(1,1,Esq) :- agregarEsqueleto([],1,EsqAux), Esq is EsqAux.
-
-
-insertarXVeces(1,X,L,[X|L]).
-insertarXVeces(N,X,L,E) :- AuxL=[X|L], R is N-1, insertarXVeces(R,X,AuxL,E).
-
-verificarR(R,N,V) :- R>=N, V is N-1,!.
-verificarR(R,_N,R).
-
-esq2(N,R,A,A).
- 
-
-esq(1,R,[[0]]) :- R>=0.
-esq(N,1,E) :- N>0, AuxN is N-1, insertarXVeces(AuxN,[1],[[0]],E).
-esq(N,R,E) :- integer(N),
-		N>0, R>0, 
-		verificarR(R,N,AuxR), AuxN is N-1,
-		esq2(AuxN,AuxR,[[AuxR]],E),!, esq(AuxN,R,E1).
